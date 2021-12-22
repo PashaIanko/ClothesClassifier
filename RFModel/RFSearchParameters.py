1bb1b8144367d0aae54b30fbc1f91f950cad10a5
PashaIanko <55174155+PashaIanko@users.noreply.github.com>
2021-12-22 18:21:34 +0100

    Update RFSearchParameters.py

--- RFModel/RFSearchParameters.py
+++ RFModel/RFSearchParameters.py
@@ -4,9 +4,6 @@ class RFSearchParameters(GridSearchParameters):
     def __init__(self):
         super().__init__()
 
-    def init_gs_params(self):
-        self.gs_params = {
-            'verbose': 0,
-            'cv': 5,
-            'n_jobs': -1
-        }
+    def update_gs_params(self):
+        # No update, keep same search parameters as in parent
+        self.gs_params.update({})  
