f6badfddf2d4d2d0feaabadfa0e17cb9ab843250
PashaIanko <55174155+PashaIanko@users.noreply.github.com>
2021-12-24 11:17:04 +0100

    Update XGBGridProperties.py

--- XGBModel/XGBGridProperties.py
+++ XGBModel/XGBGridProperties.py
@@ -6,7 +6,7 @@ class XGBGridProperties(GridProperties):
 
     def init_grid(self):
         self.grid_dict = {
-            'learning_rate': [0.1, 0.01],
+            'learning_rate': [0.1, 0.05],
             'n_estimators': [100, 200, 400],
             'subsample': [1.0, 0.85],
             'max_depth': [3, 5],
